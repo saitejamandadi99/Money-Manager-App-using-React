220c2bd2223ebdd99e55c94b9f95a5a7b39d329b
saitejamandadi99 <saitejamandadi99>
2024-09-18 08:40:39 +0000

    added user container

--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -1,3 +1,6 @@
+import {Component} from 'react'
+import './index.css'
+
 const transactionTypeOptions = [
   {
     optionId: 'INCOME',
@@ -10,3 +13,29 @@ const transactionTypeOptions = [
 ]
 
 // Write your code here
+class MoneyManager extends Component {
+  render() {
+    return (
+      <div className="app-Container">
+        <div className="money-Manager-App">
+          <div className="user-Container">
+            <h1 className="heading">Hi, Richard</h1>
+            <p className="welcome">
+              Welcome back to your <span className='spanEle'>Money Manager</span>
+            </p>
+          </div>
+          <ul className="transaction-Item-Container"></ul>
+          <div className="transaction-history-Container">
+            <form></form>
+            <div className="history-Container">
+              <div className="history-Title-Container"></div>
+              <ul className="history-Container"></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MoneyManager
